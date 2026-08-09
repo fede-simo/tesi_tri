1369a21e16de178773b8e9065ce48cec13f0cc4d
fede-simo <simonettofede@gmail.com>
2026-08-09 15:35:38 +0200

    esempio test

--- thesis/files/code/example.py
+++ thesis/files/code/example.py
@@ -1,12 +1,33 @@
-def recur_fibo(n):
-   if n <= 1:
-       return n
-   else:
-       return(recur_fibo(n-1) + recur_fibo(n-2))
-nterms = 10
-if nterms <= 0:
-   print("Plese enter a positive integer")
-else:
-   print("Fibonacci sequence:")
-   for i in range(nterms):
-       print(recur_fibo(i))
+test('checks_late_buy_orders', function (int $remainingQty, int $days, ?int $threshold, int $expectedCount) {
+
+    $user = User::factory()->create();
+
+    $buyOrder = BuyOrder::factory()->create();
+
+    AlertSetting::factory()->create([
+        'rule_name' => 'late_buy_orders',
+        'user_id' => $user->id,
+        'rule_params' => [
+            'min_time_threshold' => $threshold,
+        ],
+    ]);
+
+    BuyOrderArticle::factory()->create([
+        'buy_order_id' => $buyOrder->id,
+        'remaining_qty' => $remainingQty,
+        'requested_delivery_date' => now()->addDays($days),
+    ]);
+
+    $result = app(LateBuyOrdersRule::class)->check($user);
+
+    expect($result->count())->toBe($expectedCount);
+
+})->with([
+    [10, -1, 30, 1],    // scaduto ieri
+    [0, -1, 30, 0],     // scaduto ieri ma con tutto consegnato
+    [10, -40, 30, 0],   // scaduto 40gg fa
+    [10, -20, 30, 1],   // scaduto 20gg fa
+    [10, 1, 30, 0],     // scade domani
+    [10, 0, null, 0],   // scade oggi
+    [10, -30, 0, 0],    // scaduto 30 gg fa ma vengono presi solo quelli negli ultimi 0 giorni
+]);
